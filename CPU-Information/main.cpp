aecba48f8927d4e9983cb8d923a21b582cb0f719
Forgive <forgive0@hotmail.com>
2012-02-11 16:09:19 -0300

    Added some files plus proyect start...

---
+++ CPU-Information/main.cpp
@@ -0,0 +1,20 @@
+/*
+
+Author: Forgive
+Website: www.github.com/Forgive
+Creation Date: 11 - 02 - 2012 (15:14) (By Forgive)
+Creation Date: 11 - 02 - 2012 (15:16) (By Forgive)
+
+Here we'll put all the functions, include the headers, etc to retrieve the data.
+
+*/
+
+#pragma once
+
+#include <Windows.h>
+
+int main() {
+
+	system("PAUSE");
+	return 0;
+}
